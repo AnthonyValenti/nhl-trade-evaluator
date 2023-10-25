85dd329df560ad19c31879dbe1b2a0c30e93e4bf
Anthony Valenti <57304403+AnthonyValenti@users.noreply.github.com>
2023-10-25 19:14:34 -0400

    logic implemented plus css

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
 import "./Table.css";
 import allnames from "../data/all_names.txt";
+import playerData from "../data/playerData.json";
 
 const Table = () => {
   const [totalWar, setWar] = useState(0);
   const [players, setPlayers] = useState([]);
+  const [playersIds, setPlayersIds] = useState([]);
   const [dataArray, setDataArray] = useState([]);
   const [searchSuggestion, setSearchSuggestion] = useState([]);
   const [enteredName, setEnteredName] = useState("");
-  const [isSugVisible, setSugVisible] = useState(false)
+  const [isSugVisible, setSugVisible] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -24,8 +26,17 @@ const Table = () => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    let sum=0;
+    for (const player of players){
+      sum+=player.war;
+    }
+    setWar(Number(sum.toFixed(2)));
+
+  }, [players]);
+
   const handleInput = (event) => {
-    setSugVisible(true)
+    setSugVisible(true);
     setEnteredName(event.target.value);
     const filteredData = dataArray.filter((item) =>
       item.toLowerCase().includes(enteredName.toLowerCase())
@@ -34,43 +45,58 @@ const Table = () => {
   };
 
   const addPlayer = () => {
-    let newPlayer = {
-      name: enteredName,
-      evo: 3.1,
-      evd: 2.1,
-      war: "5.0",
-    };
-    setPlayers([...players, newPlayer]);
-    setEnteredName("");
+    if (enteredName === "") {
+      window.alert("Invalid name!");
+    } else {
+      getPlayerID();
+      let newPlayer = {
+        name: enteredName,
+        team: 0,
+        position: 0,
+        evo: 0,
+        evd: 0,
+        war: 0,
+        cap: 0,
+      };
+      playerData.rows.filter((player) => {
+        if (player[0] == enteredName) {
+          newPlayer.team = player[1];
+          newPlayer.position = player[2];
+          newPlayer.evo = Number(player[3].toFixed(2));
+          newPlayer.evd = Number(player[4].toFixed(2));
+          newPlayer.war = Number(player[5].toFixed(2));
+          newPlayer.cap = player[6];
+        }
+      });
+      setPlayers([...players, newPlayer]);
+      setEnteredName("");
+    }
   };
 
   const removePlayer = (index) => {
-    console.log(index);
     const updatedPlayers = [...players];
     updatedPlayers.splice(index, 1);
     setPlayers(updatedPlayers);
+    const updatedPlayersIds = [...playersIds];
+    updatedPlayersIds.splice(index, 1);
+    setPlayersIds(updatedPlayersIds);
   };
 
-  const getPlayerID = async () => {
-    let playerID;
-    let teamID;
-    await fetch("https://statsapi.web.nhl.com/api/v1/teams?expand=team.roster")
+  const getPlayerID = () => {
+    fetch("https://statsapi.web.nhl.com/api/v1/teams?expand=team.roster")
       .then((response) => response.json())
       .then((data) => {
         let teams = data.teams;
-
         for (let entry of teams) {
           let roster = entry.roster.roster;
-
           for (let player of roster) {
-            if (player.person.fullName === "Auston Matthews") {
-              playerID = player.person.id;
-              teamID = entry.id;
+            if (player.person.fullName === enteredName) {
+              setPlayersIds([...playersIds, player.person.id]);
+              break;
             }
           }
         }
       });
-    console.log(playerID, teamID);
   };
 
   return (
@@ -81,13 +107,12 @@ const Table = () => {
         </div>
         <div className="tradePieceContainer">
           {players.map((player, index) => (
-            <div className="tradePiece" onClick={() => removePlayer(index)}>
-              <img src="http://nhl.bamcontent.com/images/headshots/current/168x168/8479318.png" />
+            <div className="tradePiece" key={index} onClick={() => removePlayer(index)}>
+              <img src={`http://nhl.bamcontent.com/images/headshots/current/168x168/${playersIds[index]}.png`} />
               <h3>{player.name}</h3>
               <h3>EVO: {player.evo}</h3>
               <h3>EVD: {player.evd}</h3>
               <h3>WAR: {player.war}</h3>
-
             </div>
           ))}
         </div>
@@ -98,11 +123,19 @@ const Table = () => {
             className="playerSearch"
             value={enteredName}
             onChange={handleInput}
-            placeholder="Enter name"
+            placeholder="Player"
           ></input>
-          {isSugVisible & enteredName!=""? (
-            <div className="suggestionBox" onClick={()=>{setEnteredName(searchSuggestion[0]); setSugVisible(false)}}>{searchSuggestion[0]}</div>
-          ): null}
+          {isSugVisible & (enteredName !== "") ? (
+            <div
+              className="suggestionBox"
+              onClick={() => {
+                setEnteredName(searchSuggestion[0]);
+                setSugVisible(false);
+              }}
+            >
+              {searchSuggestion[0]}
+            </div>
+          ) : null}
         </div>
         <button className="addButton" onClick={addPlayer}>
           Add
